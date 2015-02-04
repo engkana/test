e686a29fe86208a22058749330d2ac9c397cf048
seigof <seigo@ayu.ics.keio.ac.jp>
2015-02-04 17:41:25 +0900

    modified

--- get_test.cpp
+++ get_test.cpp
@@ -5,6 +5,6 @@ int main(){
 	float x=1.;
 	float y=1.;
 	std::cin >> x >> y;
-	std::cout << (0.05*x-100.0) << ":" << (-0.05*y+100.0) << std::endl;
+	std::cout << (0.15*x-100.0) << ":" << (-0.3*y+100.0) << std::endl;
 	return 0;
 }
